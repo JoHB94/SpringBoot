68584b166651c1d24edbccfb0b913f9c44d07c1e
JoHB94 <whgusqls113@gmail.com>
2024-02-06 19:42:06 +0900

    service 객체 test 코드 추가

---
+++ test/src/test/java/com/springboot/test/service/impl/ProductServiceTest2.java
@@ -0,0 +1,84 @@
+package com.springboot.test.service.impl;
+
+import com.springboot.test.data.dto.ProductDto;
+import com.springboot.test.data.dto.ProductResponseDto;
+import com.springboot.test.data.entity.Product;
+import com.springboot.test.data.repository.ProductRepository;
+import com.springboot.test.service.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+@Import({ProductServiceImpl.class})
+class ProductServiceTest2 {
+
+    @MockBean
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductService productService;
+
+    @BeforeEach
+    public void setUpTest() {
+        productService = new ProductServiceImpl(productRepository);
+    }
+
+    @Test
+    void getProductTest() {
+        //given
+        Product givenProduct = new Product();
+        givenProduct.setNumber(123L);
+        givenProduct.setName("펜");
+        givenProduct.setPrice(1000);
+        givenProduct.setStock(1234);
+
+        Mockito.when(productRepository.findById(123L))
+                .thenReturn(Optional.of(givenProduct));
+
+        //when
+        ProductResponseDto productResponseDto = productService.getProduct(123L);
+
+        //then
+        Assertions.assertEquals(productResponseDto.getNumber(),givenProduct.getNumber());
+        Assertions.assertEquals(productResponseDto.getName(),givenProduct.getName());
+        Assertions.assertEquals(productResponseDto.getPrice(),givenProduct.getPrice());
+        Assertions.assertEquals(productResponseDto.getStock(),givenProduct.getStock());
+
+        verify(productRepository).findById(123L);
+    }
+
+    @Test
+    void saveProductTest() {
+        Mockito.when(productRepository.save(any(Product.class))).then(returnsFirstArg());
+        /*
+         * any() : Mockito의 ArgumentMatchers에서 제공하는 메서드로
+         * 목 객체의 동작을 정의 하거나 검증하는 단계에서 조건으로 특정 매개변수의 전달을 설정하지 않고
+         * 메서드의 실행만을 확인 하거나 좀 더 큰 범위의 클래스 객체를 매개변수로 전달받는 등의 상황에 사용
+         * save의 매개변수로 any(Product.class)로 동작을 설정 했는데,
+         * 일반적으로 given()으로 정의 된 Mock객체의 메서드 동작 감지는 매개변수의 비교를 통해 이루어지나
+         * 레퍼런스 변수의 비교는 주솟값으로 이뤄지기 때문에 any()를 사용해 클래스만 정의하는 경우도 있다.
+         * */
+        ProductResponseDto productResponseDto = productService.saveProduct(
+                new ProductDto("펜",1000,1234));
+
+        Assertions.assertEquals(productResponseDto.getName(),"펜");
+        Assertions.assertEquals(productResponseDto.getPrice(),1000);
+        Assertions.assertEquals(productResponseDto.getStock(),1234);
+
+        verify(productRepository).save(any());
+
+    }
+}
